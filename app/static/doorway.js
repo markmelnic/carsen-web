7ae8b1a1e309c7a5ec6c657a8392e82f2ab84bed
Mark Melnic <markhappy200@gmail.com>
2020-11-11 19:18:18 +0100

    Minor changes and restructuration

--- app/static/doorway.js
+++ app/static/doorway.js
@@ -1,3 +1,5 @@
+// credentials box handlers
+
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
