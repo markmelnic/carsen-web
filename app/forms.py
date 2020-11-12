8ff09351d4a4f01f9226ffdaf3d25a685c44c2f9
Mark Melnic <markhappy200@gmail.com>
2020-11-12 10:42:26 +0100

    Added search results loader

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from app.models import User
 
@@ -25,3 +25,18 @@ class RegisterForm(FlaskForm):
         user_email = User.query.filter_by(email=email.data).first()
         if user_email:
             raise ValidationError("Email already in use, please choose another")
+
+class SearchForm(FlaskForm):
+    manufacturer = StringField("Manufacturer", validators=[DataRequired()])
+    model = StringField("Model", validators=[DataRequired()])
+
+    price_from = IntegerField("From", validators=[])
+    price_to = IntegerField("To", validators=[])
+
+    reg_from = IntegerField("From", validators=[])
+    reg_to = IntegerField("To", validators=[])
+
+    mileage_from = IntegerField("From", validators=[])
+    mileage_to = IntegerField("To", validators=[])
+
+    submit = SubmitField("Search")
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
-from mobile_de.scraper import next_page
-from flask import request, render_template, url_for, redirect
+from mobile_de.methods import surface_search
+from flask import request, render_template, url_for, redirect, flash, get_flashed_messages, jsonify
 from app import app, db, bcrypt
-from app.forms import LoginForm, RegisterForm
+from app.forms import LoginForm, RegisterForm, SearchForm
 from app.models import User
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -14,7 +14,7 @@ def home():
 @app.route("/doorway")
 def doorway():
     if current_user.is_authenticated:
-        return redirect(url_for("Dashboard"))
+        return redirect(url_for("dashboard"))
     login_form = LoginForm()
     register_form = RegisterForm()
     return render_template(
@@ -25,19 +25,21 @@ def doorway():
 @app.route("/login", methods=["POST"])
 def login():
     login_form = LoginForm()
-    register_form = RegisterForm()
     if login_form.validate_on_submit():
         user = User.query.filter_by(email=login_form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, login_form.password.data):
             login_user(user)
             next_page = request.args.get("next")
-            return redirect(next_page) if next_page else redirect(url_for("Dashboard"))
+            return redirect(next_page) if next_page else redirect(url_for("dashboard"))
+        else:
+            flash(u'Incorrect password - email combination!', "login_error")
+            return redirect(url_for("doorway"))
+    flash(u'User not found!', "login_error")
     return redirect(url_for("doorway"))
 
 
 @app.route("/register", methods=["POST"])
 def register():
-    login_form = LoginForm()
     register_form = RegisterForm()
     if register_form.validate_on_submit():
         hashed_pass = bcrypt.generate_password_hash(login_form.password.data).decode("utf-8")
@@ -46,9 +48,9 @@ def register():
         db.session.commit()
         login_user(user)
         next_page = request.args.get("next")
-        return redirect(next_page) if next_page else redirect(url_for("Dashboard"))
-    else:
-        return redirect(url_for("doorway"))
+        return redirect(next_page) if next_page else redirect(url_for("dashboard"))
+    flash(u'Invalid credentials provided!', "register_error")
+    return redirect(url_for("doorway"))
 
 @app.route("/logout")
 def logout():
@@ -57,6 +59,19 @@ def logout():
 
 @app.route("/dashboard", methods=["GET", "POST"])
 @login_required
-def Dashboard():
+def dashboard():
     title = "Dashboard - " + str(current_user.name)
-    return render_template("dashboard.html", page="dash", title=title)
+    search_form = SearchForm()
+    return render_template("dashboard.html", page="dash", title=title, search_form=search_form)
+
+@app.route("/search", methods=["POST"])
+@login_required
+def search():
+    title = "Dashboard - " + str(current_user.name)
+    search_form = SearchForm()
+    manufacturer = search_form.manufacturer.data
+    model = search_form.model.data
+    results = surface_search([manufacturer, model, '', '', '', '', '', ''])
+    #return redirect(url_for("dashboard", results=results))
+    return render_template("dashboard.html", page="dash", title=title, search_form=search_form, results=results)
+
