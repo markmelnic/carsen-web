543e2d31a53cb5026feddd7e3ffd16003db04641
Mark Melnic <markhappy200@gmail.com>
2020-11-13 15:30:57 +0100

    Implemented all favorite adding functionality - unduplicated records in the database - multiple users with unique or shared records

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 from mobile_de.methods import surface_search
+from json import loads
 from flask import (
     request,
     render_template,
@@ -10,9 +11,67 @@ from flask import (
 )
 from app import app, db, bcrypt
 from app.forms import LoginForm, RegisterForm, SearchForm
-from app.models import User
+from app.models import User, Favorite
 from flask_login import login_user, current_user, logout_user, login_required
 
+db.create_all()
+db.session.commit()
+
+def add_favorites(fav):
+    find_dup = Favorite.query.filter_by(
+        image=fav["image"],
+        title=fav["title"],
+        price=fav["price"],
+        reg=fav["reg"],
+        mileage=fav["mileage"],
+    ).first()
+    if find_dup != None:
+        current_favs = current_user.favorites
+        if not str(find_dup.id) in current_favs.split("|"):
+            if current_favs == "":
+                current_user.favorites = current_favs + str(find_dup.id)
+            else:
+                current_user.favorites = current_favs + "|" + str(find_dup.id)
+            db.session.commit()
+    else:
+        fav_db = Favorite(
+            url=fav["url"],
+            image=fav["image"],
+            title=fav["title"],
+            price=fav["price"],
+            reg=fav["reg"],
+            mileage=fav["mileage"],
+            user_added=current_user.id,
+        )
+        db.session.add(fav_db)
+        db.session.flush()
+
+        current_favs = current_user.favorites
+        if current_favs == "":
+            current_user.favorites = current_favs + str(fav_db.id)
+        else:
+            current_user.favorites = current_favs + "|" + str(fav_db.id)
+
+        db.session.commit()
+
+def get_favorites():
+    if current_user.favorites == "":
+        return ['']
+    else:
+        favorites = current_user.favorites.split("|")
+        favs = []
+        for i in favorites:
+            fav = Favorite.query.get(i)
+            favs.append([
+                    fav.url,
+                    fav.title,
+                    fav.price,
+                    fav.reg,
+                    fav.mileage,
+                    fav.image,
+                ])
+
+    return favs
 
 @app.route("/")
 def home():
@@ -82,9 +141,10 @@ def logout():
 @login_required
 def dashboard():
     title = "Dashboard - " + str(current_user.name)
+    favs = get_favorites()
     search_form = SearchForm()
     return render_template(
-        "dashboard.html", page="dash", title=title, search_form=search_form
+        "dashboard.html", page="dash", title=title, search_form=search_form, favs=favs
     )
 
 
@@ -94,20 +154,33 @@ def search():
     try:
         results = surface_search(
             [
-                request.form['manufacturer'],
-                request.form['model'],
-                request.form['price_from'],
-                request.form['price_to'],
-                request.form['reg_from'],
-                request.form['reg_to'],
-                request.form['mileage_from'],
-                request.form['mileage_to'],
+                request.form["manufacturer"],
+                request.form["model"],
+                request.form["price_from"],
+                request.form["price_to"],
+                request.form["reg_from"],
+                request.form["reg_to"],
+                request.form["mileage_from"],
+                request.form["mileage_to"],
             ]
         )
     except AssertionError:
         results = []
-        flash(u"Your search did not yield any results. Try other search parameters.", "no_search_results")
+        flash(
+            u"Your search did not yield any results. Try changing the parameters.",
+            "no_search_results",
+        )
     return render_template(
         "results.html",
         results=results,
     )
+
+
+@app.route("/add_to_favorites", methods=["GET", "POST"])
+@login_required
+def add_to_favorites():
+    fav = loads(request.form.to_dict()["qSet"])
+    add_favorites(fav)
+
+    favs = get_favorites()
+    return render_template("favorites.html", favs=favs)
